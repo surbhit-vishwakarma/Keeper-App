3c1f8b51a422d33545f6b199ee23cac64324a985
Noob <107372384+surbhit-vishwakarma@users.noreply.github.com>
2024-03-28 05:41:30 +0530

    Delete keeper-app directory

--- keeper-app/src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Header.css'
-
-function Header()
-{
-    return <h1 className="headers"><span className="header-text">Keeper App</span></h1>
-}
-
-export default Header
